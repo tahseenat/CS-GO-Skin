a1ffee8c764dd750cd3bf53fb40a765b738740c6
tarun <gargtarun08@gmail.com>
2020-02-23 11:14:30 +0530

    skin_details.py Updated

--- skin_details.py
+++ skin_details.py
@@ -1,11 +1,18 @@
-import json
-from bs4 import BeautifulSoup
 import requests
-
+#def get_src(name):
 URL = "https://csgoempire.com/api/v2/inventory/site/10"
 r = requests.get(URL).json()
-print(r[0])
-
+details=[]
 for i in range(len(r)):
     if r[i]["appid"]==570:
-        print(r[i]["name"],"-------",r[i]["market_value"]/100)
+        details.append(r[i])
+price=10000000
+src="na"
+name=input("Enter skin name:")
+for i in range(len(details)):
+    if details[i]["name"].lower()==name.lower():
+        if details[i]["market_value"]<price:
+            price=details[i]["market_value"]
+            src=details[i]['icon_url']
+print(price/100,'\n',src)
+#return src
