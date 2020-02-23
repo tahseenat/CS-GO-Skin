9af0ba7ab55cedd69bc6858514e4847aa65990c0
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2020-02-23 20:05:14 +0530

    Buy skins function added, login added Need to read csv and integrate now.

--- credentials.py
+++ credentials.py
@@ -2,3 +2,6 @@ username = "test88787"
 password = "82175423"
 
 steam_url = "https://steamcommunity.com/tradeoffer/new/?partner=1067794763&token=8lXOYhKa"
+
+
+cs_go_home_url = "https://csgoempire.com/withdraw"
--- home.py
+++ home.py
@@ -3,18 +3,50 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 import csv
-import autoit
-import keyboard
+from credentials import password, username, cs_go_home_url, steam_url
+from skin_details import get_src
+
+
+def buy(skin_name):
+    driver.find_element(By.XPATH, """(//*[contains(text(), '{}')])[1]""".format(skin_name)).click()
+    driver.find_element(By.XPATH, """(//*[contains(text(), 'Withdraw')])[2]""").click()
+    driver.find_element(By.XPATH, """//*[@id="trade-url"]""").send_keys(steam_url)
+    driver.find_element(By.XPATH, """(//*[contains(text(), 'Confirm')])[1]""").click()
+
 
 # if __name__ == "__main__":
-cs_go_home = "https://csgoempire.com/withdraw"
 
 # specify waits in sec
 soft_wait = 2
 long_wait = 4
+dota_wait = 1.2
+refresh_wait = 1
 
 driver = webdriver.Chrome(executable_path='D:/Downloads/chromedriver_win32/chromedriver.exe')
 
-driver.get(cs_go_home)
+driver.get(cs_go_home_url)
 
 time.sleep(long_wait + soft_wait)
+driver.find_element(By.XPATH, """(//*[contains( text(), "Sign In")])[1]""").click()
+time.sleep(long_wait)
+
+driver.find_element(By.XPATH, """//*[@id = "steamAccountName"]""").send_keys(username)
+driver.find_element(By.XPATH, """//*[@id="steamPassword"]""").send_keys(password)
+driver.find_element(By.XPATH, """//*[@id="imageLogin"]""").click()
+
+time.sleep(long_wait)
+
+driver.find_element(By.XPATH, """//*[contains(text(), 'Continue')]""").click()
+driver.find_element(By.XPATH, """(//*[contains(text(), 'Withdraw')])[1]""").click()
+
+skin_name = "Golden Staff"
+
+while True:
+    driver.refresh()
+    time.sleep(refresh_wait)
+    try:
+        driver.find_element(By.XPATH, """(//*[contains(text(), 'Dota')])[1]""").click()
+        buy(skin_name)
+    except:
+        pass
+    time.sleep(dota_wait)
