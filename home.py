482db463932ad81fcf1158ad25baa0a3cdc0e5a2
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2020-03-01 17:26:36 +0530

    Added the number of item to buy functionality

--- home.py
+++ home.py
@@ -6,12 +6,14 @@ from credentials import cs_go_home_url, steam_url, withdraw_url, exe_path
 from skin_details import get_src
 
 
-def add_skin(skin_name):
-    print(skin_name)
-    item = "(//*[contains(text(), '{}')])[1]".format(skin_name)
+def add_skin(i):
+    item = "(//*[contains(text(), '{}')])[1]".format(data['skin_name'][i])
     try:
         driver.find_element(By.XPATH, item).click()
+        data['no_of_skin'][i] -= 1
+
     except Exception as e:
+        print("Exception adding skin")
         print(e)
 
 
@@ -33,10 +35,13 @@ def login():
 def buy():
     try:
         driver.find_element(By.XPATH, """(//*[contains(text(), 'Withdraw')])[2]""").click()
-        driver.find_element(By.XPATH, """//*[@id="trade-url"]""").send_keys(steam_url)
+        try:
+            driver.find_element(By.XPATH, """//*[@id="trade-url"]""").send_keys(steam_url)
+        except:
+            pass
         driver.find_element(By.XPATH, """(//*[contains(text(), 'Confirm')])[1]""").click()
     except Exception as e:
-        print(e)
+        print("Exception in Buy functionality", e)
 
 
 if __name__ == "__main__":
@@ -47,7 +52,9 @@ if __name__ == "__main__":
     dota_wait = 2.5
     refresh_wait = 1.4
     hold_page = 0.3
-    login_wait = 40
+    login_wait = 4
+
+    file_name = "Skins.csv"
 
     driver = webdriver.Chrome(executable_path=exe_path)
 
@@ -57,7 +64,7 @@ if __name__ == "__main__":
     time.sleep(login_wait)
 
     # skin_name = "Golden Staff"
-    data = pd.read_csv("Skins.csv")
+    data = pd.read_csv(file_name)
     print(data)
     driver.execute_script("window.open('');")
     driver.switch_to.window(driver.window_handles[1])
@@ -69,12 +76,19 @@ if __name__ == "__main__":
         try:
             driver.find_element(By.XPATH, """(//*[contains(text(), 'Dota')])[1]""").click()
             time.sleep(dota_wait)
-            # data frame read and pass the name of skin in buy function
-            for i in data.index:
-                if data['Flag'][i] == "Not Sent":
-                    add_skin(data['Skin_name'][i])
+            for page in range(2):
+                # data frame read and pass the name of skin in buy function
+                for i in data.index:
+                    if data['no_of_skin'][i] > 0:
+                        add_skin(i)
+                try:
+                    driver.find_element(By.XPATH, "//*[contains(text(), 'Next')]").click()
+                    time.sleep(1.5)
+                except Exception as e:
+                    print(e)
 
         except Exception as e:
             print(e)
-        # buy()
+        buy()
+        data.to_csv(file_name, index=False)
         time.sleep(dota_wait)
